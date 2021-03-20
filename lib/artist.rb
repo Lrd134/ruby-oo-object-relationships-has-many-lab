6e2666fc744e427efe15ffca671ce9d6ed7a4165
Lrd134 <larryc3200@gmail.com>
2021-03-19 21:45:16 -0400

    commit tree 1586553f0921fbbdea1557eb125681d308d9a1f7

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,22 +4,18 @@ class Artist
   
     def initialize(name) #When we make an aritst
         @name = name # add the given name to the instance of name
-        @songs = [] # initialize the instance of songs to an empty array
     end
     def add_song(song)
-        binding.pry
         song.artist = self # set the artists name = to the Artist instance
-        @songs << song # shovel the song instance into the instance of the songs array
     end
     def songs
-        @songs #reader for the songs array
+        Song.all.select { | obj | obj.artist == self } #reader for the songs array
     end
     def add_song_by_name(string_song)
         song = Song.new(string_song) #same as add_song but we need to make the song class ourself.
         song.artist = self
-        @songs << song
     end
-    def self.song_count 
-        @songs.size # returns the size of the songs array
-    end
+     def self.song_count 
+        Song.all.size # returns the size of the songs array
+     end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,39 @@
+# class Song
+#     attr_accessor :name
+#     @@all = []
+#     def artist=(artist)
+#         @artist = artist
+#     end
+#     def artist
+#         @artist
+#     end
+#     def initialize(title)
+#         @name = title
+#         @@all << self
+#     end
+#     def artist_name
+#         @artist_name
+#     end
+#     def self.all
+#         @@all
+#     end
+# end
 class Song
-    attr_accessor :name
+    attr_accessor :name, :artist
+  
     @@all = []
-    def artist=(artist)
-        @artist = artist
+  
+    def initialize(name)
+      @name = name
+      @@all << self
     end
-    def artist
-        @artist
-    end
-    def initialize(title)
-        @name = title
-        @@all << self
-    end
-    def artist_name
-        @artist_name
-    end
+  
     def self.all
-        @@all
+      @@all
     end
-end
+  
+    def artist_name
+      @artist.name if @artist
+    end
+  end
+  
